edb9dd00b71182a382a0e8ccc5a72e95e10d6951
Stephanie Spencer <Stephanie.Spencer.22@hotmail.com>
2020-05-27 10:29:18 -0600

    remade

--- Bowling Scores/ViewController.swift
+++ Bowling Scores/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var game1: UITextField!
+    @IBOutlet weak var game2: UITextField!
+    @IBOutlet weak var game3: UITextField!
+    
+    @IBOutlet weak var series: UILabel!
+    @IBOutlet weak var average: UILabel!
+    @IBOutlet weak var high: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calculate(_ sender: Any) {
+    }
+    
 }
 
